cadae19f4f32868488f645c97790c63528855d28
rower19941 <rower19941@gmail.com>
2016-09-21 15:59:51 +0400

    Application class added

---
+++ src/main/java/Application.java
@@ -0,0 +1,25 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Created by Валерий on 21.09.2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(){
+        return (args -> {
+
+        });
+    }
+}
